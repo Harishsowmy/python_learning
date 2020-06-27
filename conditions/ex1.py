ec3e2119b4e01186bb46b812b72c7eaf4d1be3bf
Harish Sowmy <harishsowmy@Harishs-MacBook-Air.local>
2020-06-27 10:38:29 -0500

    Adding conditions

---
+++ conditions/ex1.py
@@ -0,0 +1,12 @@
+cats=10
+dogs=2
+people=250
+if cats < dogs:
+    print("cats are lees than dogs")
+
+if  cats > dogs:
+    print("cats are grater than dogs")
+
+if cats < people:
+
+    print("people are greater than cats")
---
+++ conditions/ex2.py
@@ -0,0 +1,18 @@
+people=10
+dogs=100
+cats=29
+
+if people > dogs:
+    print("dogs are more than people")
+
+if people==cats: 
+    print("people and cats are equal")
+
+if people<dogs:
+    print("people are more than dogs")
+
+if people<dogs and people<cats:
+    print("people are less than dogs and cats")
+
+if people<dogs or people>cats:
+    print("people are less than dogs or people are greater than cats0")
